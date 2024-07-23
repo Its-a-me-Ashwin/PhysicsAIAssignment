d56ede50cbb7cae20023ee6b2e703bb19a978dfe
Its-a-me-Ashwin <ashwinrb7799@gmail.com>
2024-07-22 21:20:01 -0400

    Load fix

--- Model/createKANdataset.py
+++ Model/createKANdataset.py
@@ -37,8 +37,19 @@ def create_encoded_dataset(graph_dataset, encoder, device='cpu'):
 
 # Example usage
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+# Load the full state dictionary
+full_state_dict = torch.load("../models/model_h8192_lr0.005_wd0_e50.pt")
+
 trained_encoder = GAEEncoder(in_channels=4, hidden_channels=8192, out_channels=512)
-trained_encoder.load_state_dict(torch.load("../models/model_h8192_lr0.0005_wd0_e50.pt"))
+
+# Extract the encoder part of the state dictionary
+encoder_state_dict = {k.replace("encoder.", ""): v for k, v in full_state_dict.items() if k.startswith("encoder.")}
+
+# Load the encoder state dictionary into the encoder model
+trained_encoder.load_state_dict(encoder_state_dict)
+
 new_dataset = create_encoded_dataset(graph_dataset, trained_encoder, device=device)
 
 torch.save(new_dataset, "./Dataset/kanDataSet.pt")
